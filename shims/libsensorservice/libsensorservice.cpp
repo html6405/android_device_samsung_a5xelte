0b357198768c90fdec6c372cac608da81b38aade
html6405 <peter.schelchshorn@mhs-solutions.at>
2023-09-20 17:27:32 +0200

    libsensors: shim _ZN7android15checkPermissionERKNS_8String16Eij to make libsensorservice happy

--- shims/libsensorservice/libsensorservice.cpp
+++ shims/libsensorservice/libsensorservice.cpp
@@ -6,3 +6,6 @@ extern "C" void _ZN7android13SensorService26addProximityActiveListenerERKNS_2spI
 
 extern "C" void _ZN7android13SensorService29removeProximityActiveListenerERKNS_2spINS0_23ProximityActiveListenerEEE();
 extern "C" void _ZN7android13SensorService29removeProximityActiveListenerERKNS_2spINS0_23ProximityActiveListenerEEE(){}
+
+extern "C" void _ZN7android15checkPermissionERKNS_8String16Eij();
+extern "C" void _ZN7android15checkPermissionERKNS_8String16Eij(){}
